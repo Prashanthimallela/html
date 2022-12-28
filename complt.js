a93b6183c2cba4cebe87e7cb8dfdd7f3068d472a
Prashanthimallela <119045241+Prashanthimallela@users.noreply.github.com>
2022-12-28 09:13:57 +0530

    Add files via upload

---
+++ complt.js
@@ -0,0 +1,2 @@
+var result =localStorage.getItem("output1")
+document.getElementById("pd1").innerHTML=result;
--- registration.js
+++ registration.js
@@ -1,94 +1,40 @@
-
+let array=[]
 function validation(){
-  var usid = document.registration.userid;
-  var usname=document.registration.name;
-  var password=document.registration.password;
-  var repeatpassword=document.registration.repeatpassword;
-  var Dob=document.registration.DateofBirth;
-  var add=document.registration.Address;
-  var edqifs=document.registration.EducationalQualification;
-  var hobb =document.registration.Hobbies;
-  var listqif=document.registration.listofCertifications;
- 
-function usid_validation(usid,ax,ay)
-{
-  var usid_len = usid.value.length;
-  if (usid_len == 0 || usid_len >= ay || usid_len < ax)
-  {
-   alert("User Id should not be empty / length  between "+ax+" to "+ay);
-    usid.focus();
-  return false;
+  var usid = document.getElementById("userid").value;
+  var usname=document.getElementById("name").value;
+  var password=document.getElementById("psw").value;
+  var repeatpassword=document.getElementById("rpsw").value;
+  var Dob=document.getElementById("doj").value;
+  var add=document.getElementById("add").value;
+  var edqifs=document.getElementById("item").value;
+  var hobb =document.getElementById("hbs").value;
+  var listqif=document.getElementById("cert").value;
+   array.push(usname)
+   array.push(Dob)
+   array.push(add)
+   array.push(edqifs)
+   array.push(hobb)
+   array.push(listqif)
+   var output="usname:-"+array[0]+"<br>"+"Dob:-"+array[1]+"<br>"+"add:-"+array[2]+"<br>"+"edqifs:-"+array[3]+"<br>"+"hobb:-"+array[4]+"<br>"+"listqif:-"+array[5]+"<br>"
+   localStorage.setItem("output1",output)
+  if(password!=repeatpassword){
+    alert("notmatched")
+    return false;
   }
-  return true;
-  }
-  
-  {
-    if(usid_validation(usid,5,10))
-    {
-      if(allLetter(usid))
-      {
-
-      }
-    }
+  else{
+    return true;
   }
 
 
-function usname_validation(usname,ax,ay){
-  var usname_len = usname.value.length;
-  if (usname_len == 0 || usname_len >= ay || usname_len < ax)
-  {
 
-    alert("User name should not be empty / length  between "+ax+" to "+ay);
-    usname.focus();
-  return false;
-  }
-  return true;
-  }
-  {
-  if(usname_validation(usname,5,10))
-    {
-      if(allLetter(usname))
-      {
 
-      }
-    }
-  }
 
- /* function password_validation(password,ax,ay){
-    var password_len = password.value.length;
-    if (password_len == 0 || password_len >= ay || password_len < ax)
-    {
-  
-      alert("user password should not be empty / length  between "+ax+" to "+ay);
-      password.focus();
-     return false;
-    }
-     return true;
-    }
-    {
-    if(password_validation(password,5,10))
-      {
-        if(allLetter(password))
-        {
-        }
-      }
-    }
-
-  }*/
-  function Password(inputtxt) 
-{ 
-var password= " /^[A-Za-z]\w{7,14}$/";
-if(inputtxt.value.match(password)) 
-{ 
-alert('Correct, try another...')
-return true;
 }
-else
-{ 
-alert('Wrong...!')
-return false;
+function generateUserId() {
+  var usid = (Math.pow(10,8));
+  var userid=Math.trunc(parseInt(usid)*Math.random());
+  document.getElementById("userid").value =userid;
 }
-}
-}
+window.onload = generateUserId;
 
 
